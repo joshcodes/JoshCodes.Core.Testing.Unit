5829ea134fd11cee7401ef19f00fcd8fdb94c8a9
Josh Wingstrom <josh@magicmoments.com>
2013-08-08 15:47:19 -0400

    initial

---
+++ DictionaryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using JoshCodes.Collections.Generic;
+
+namespace JoshCodes.Core.Testing.Unit
+{
+    [TestClass]
+    public class DictionaryExtensions
+    {
+        [TestMethod]
+        public void KeysAsSeparatedString()
+        {
+            var testDictionary = new Dictionary<string, string>() {
+                {"asdf", "qwer"},
+                {"foo", "bar"},
+                {"food", "barf"}
+            };
+            var example = testDictionary.KeysAsSeparatedString(",", (s) => s);
+            Assert.AreEqual("asdf,foo,food", example);
+        }
+    }
+}
